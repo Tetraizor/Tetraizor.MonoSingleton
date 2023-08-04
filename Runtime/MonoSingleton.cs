78eec321e9f2fecd80a11ac40d74fb096b02f419
Umuthan Özel <ozelumuthan@gmail.com>
2023-08-04 23:20:24 +0300

    Implement base MonoSingleton class

--- Runtime/MonoSingleton.cs
+++ Runtime/MonoSingleton.cs
@@ -1,18 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-public class MonoSingleton : MonoBehaviour
+namespace Tetraizor.Singleton
 {
-    // Start is called before the first frame update
-    void Start()
+    public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
     {
+        private bool _isInitialized;
         
-    }
+        private static T _instance;
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindObjectOfType<T>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+                    if (_instance == null)
+                    {
+                        var newGameObject = new GameObject((string) typeof(T).Name);
+                        var newInstance = newGameObject.AddComponent<T>();
+
+                        _instance = newInstance;
+
+                        _instance.Init();
+                    }
+                }
+                
+                return _instance;
+            }
+        }
+
+        private void Awake()
+        {
+            if(!_isInitialized)
+                Init();
+        }
+
+        protected virtual void Init()
+        {
+            _isInitialized = false;
+        }
     }
 }
